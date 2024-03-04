1d384a8fb536a5f9407110fb2cf3084e2032c203
Nandhi Shree <162119615+NandhiShreeVenkatesan@users.noreply.github.com>
2024-03-04 16:52:36 +0530

    Create CovertPos2Neg.java

---
+++ CovertPos2Neg.java
@@ -0,0 +1,12 @@
+package substitute;
+
+public class CovertPos2Neg {
+	 public static void main(String[] args) {
+	      int Number ;
+	      int NegVal;
+	      NegVal = -40;
+	      System.out.println(" NEG VALUE = "+ NegVal);
+	      Number= NegVal *(-1);
+	      System.out.println(" POS VALUE = "+Number);
+	   }
+}
