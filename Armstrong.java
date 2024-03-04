8e15e179ee7626bf94958e519b7138a494318083
Nandhi Shree <162119615+NandhiShreeVenkatesan@users.noreply.github.com>
2024-03-04 16:51:31 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,22 @@
+package substitute;
+
+public class Armstrong {
+	public static void main(String[] args) {
+		int number=166;
+		int r=0,sum=0;
+		int temp=number;
+		
+		while(temp>0) {
+			r=temp%10;
+			
+			sum=sum+(r*r*r);
+			temp=temp/10;
+		}
+		if(sum==number) {
+			System.out.println("  Armstrong number:"+number);
+		}
+		else {
+			System.out.println(" not an Armstrong number:"+number);
+		}
+	}
+}
