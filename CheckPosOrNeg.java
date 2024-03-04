8ee18b613905470ec44883229714182b0734f0ed
Nandhi Shree <162119615+NandhiShreeVenkatesan@users.noreply.github.com>
2024-03-04 16:52:14 +0530

    Create CheckPosOrNeg.java

---
+++ CheckPosOrNeg.java
@@ -0,0 +1,17 @@
+package substitute;
+
+public class CheckPosOrNeg {
+	public static void main(String[] args)   
+	{  
+	int num=-6;  
+	if(num>0) {
+		//checks the number is great than 0 
+		System.out.println("Number is POSITIVE");
+	}
+	else {
+		//checks the number is less than 0 
+		System.out.println("Number is NEGATIVE");
+	}
+	
+	}
+}
